6bdb2b2c505fe4aeb38ed7f7238da4d4264c7a8c
txl19881019 <492849904@qq.com>
2019-12-03 10:50:56 +0800

    commit code

---
+++ app/src/main/java/cn/com/wupower/activity/BaseActivity.java
@@ -0,0 +1,6 @@
+package cn.com.wupower.activity;
+
+import android.app.Activity;
+
+public class BaseActivity extends Activity {
+}
---
+++ app/src/main/java/cn/com/wupower/activity/Welcome.java
@@ -0,0 +1,6 @@
+package cn.com.wupower.activity;
+
+import android.app.Activity;
+
+public class Welcome extends Activity {
+}
